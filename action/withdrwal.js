b5bb29e1dd9256e6c8a0b922a1cfca7e721585ac
NwabuezeDavid <nwabuezedavid013@gmail.com>
2026-01-12 06:44:28 -0800

    d

--- action/withdrwal.js
+++ action/withdrwal.js
@@ -51,7 +51,7 @@ if (amount >= 499 && user.kycStatus === 'PENDING'  ) {
         userId: session.id,
         amount,
         crypto: method,
-        address,
+        wallet:address,
         status: "PENDING",
       },
     }),
